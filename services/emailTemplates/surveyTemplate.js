a6d4b799607ea58ff2fa420eac734ec37f8f286f
Ajmal Jalal <ajmal@zennify.com>
2020-09-22 16:54:27 -0700

    addes styling to email templates

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,18 +1,20 @@
-module.exports = (survey)=>{
+module.exports = (survey) => {
     return `
         <html>
-            <bod>
-                <div style = "text-align: center;">
-                    <h3>Hi dear costumer!</h3>
-                    <p>Please answer the following question!</p>
-                    <p>${survey.body}</p>
-                    <div>
-                        <a href="http://localhost:5000/api/surveys/${survey.id}/yes">Yes</a>
-                    </div>
-                    <div>
-                        <a href="http://localhost:5000/api/surveys/${survey.id}/no">No</a>
-                    </div>
-                </div>
+        <bod>
+            <div style="text-align: center;
+            background: whitesmoke;
+            padding: 50px;
+            height: 100%;">
+                <h3>HELLO DEAR COSTUMER!</h3>
+                <p>Please answer the following question. This will help us understand your needs and interest better.</p>
+                <p style="margin-top: 50px; font-weight: bold; font-size: 18px;">${survey.body}</p>
+
+                <a style="text-decoration: none; color: green; margin-right: 10px; font-weight: bold"
+                    href="http://localhost:5000/api/surveys/${survey.id}/yes">YES</a>
+                <a style="text-decoration: none; color: red; font-weight: bold" href="http://localhost:5000/api/surveys/${survey.id}/no">NO</a>
+
+            </div>
             </body>
 
         </html>
