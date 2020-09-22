cbd4e491ec276a192fc4d15717869d07a04ea241
Ajmal Jalal <ajmal@zennify.com>
2020-09-22 15:04:32 -0700

    added dev config to git

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: "42760385859-favqjmnuofl24m4g82dhvlf5amvmcgqk.apps.googleusercontent.com",
+    googleClientSecret: "TiWdalkdIK79MufTKY7YxWQJ",
+    mongoURI: "mongodb://ajmal:ajmal1234@ds235461.mlab.com:35461/servegar-dev",
+    cookieKey: 'thisIsAVeryhardotomenewcookiemorizekeyherehowaboutyou',
+    stripePublishableKey: "pk_test_vpDyHT48Av0arrD43Nh9PvNv",
+    stripeSecretKey: "sk_test_xGZlcIf2C5nu50mh1r5AkcGU",
+    sendGridKey: "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"
+}
